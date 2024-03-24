0259f581b159fbd66a5bdc9be3b2832d78ea0050
marco kamal <102481965+marcokamal88@users.noreply.github.com>
2024-03-24 13:33:36 +0200

    Add files via upload
    searching using query param and fixing some errors

--- TVmaze/src/App.jsx
+++ TVmaze/src/App.jsx
@@ -4,36 +4,52 @@ import CircularProgress from "@mui/material/CircularProgress";
 import "./App.css";
 import axios from "axios";
 
+const URL = "https://api.tvmaze.com/shows";
 function App() {
   const [Shows, setShows] = useState([]);
   const [seasons, setSeasons] = useState([]);
   const [casts, setCasts] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [seasonId, setseasonId] = useState(null);
-  const [open, setOpen] = useState(true);
+  const [Loading, setLoading] = useState(null);
 
-  const URL = "https://api.tvmaze.com/shows";
-
+  const handleSearch = () => {
+    setLoading(true);
+    axios
+      .get("https://api.tvmaze.com/search/shows", {
+        params: {
+          q: searchTerm,
+        },
+      })
+      .then(function (response) {
+        console.log(response.data.map((obj) => obj.show));
+        setShows(response.data.map((obj) => obj.show));
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
   const getData = () => {
+    setLoading(true);
     axios
       .get(URL)
       .then((response) => {
         setShows(response.data);
-        // setLoading(false);
-        // console.log(Shows);
       })
       .catch((e) => {
         console.log(e);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   };
 
   useEffect(() => {
     getData();
   }, []);
-
-  const search = Shows.filter((i) =>
-    i.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
 
   const showDetails = (id) => {
     setseasonId(id);
@@ -58,16 +74,10 @@ function App() {
       });
   };
 
-  setInterval(() => {
-    setOpen(false);
-  }, 3000);
-
   return (
     <>
-      {!open ? (
+      {
         <div>
-          {/* <Button onClick={handleOpen}>Show backdrop</Button> */}
-          {console.log("2")}
           <div className="head">
             <h2>available shows is:</h2>
             <div className="search">
@@ -77,101 +87,63 @@ function App() {
                 placeholder="search"
                 onChange={(e) => setSearchTerm(e.target.value)}
               />
-              <button className="search-btn">search</button>
+              <button onClick={handleSearch} className="search-btn">
+                search
+              </button>
             </div>
           </div>
           <div className="container">
-            {searchTerm === ""
-              ? Shows.map((show) => (
-                  <div className="displayShows" key={show.id}>
-                    <div className="name-btn">
-                      <p className="showName">
-                        {" "}
-                        <span>show name:</span> {show.name}
-                      </p>
-                      <button
-                        className="details-btn"
-                        onClick={() => showDetails(show.id)}
-                      >
-                        show details
-                      </button>
-                    </div>
-                    <div className="details">
-                      {seasons === ""
-                        ? null
-                        : seasonId === show.id
-                        ? seasons.map((season) => (
-                            <div key={season.id} className="seasons">
-                              <p className="seasonsNumber">
-                                season: {season.number}
-                              </p>
-                            </div>
-                          ))
-                        : null}
+            {Shows.map((show) => (
+              <div className="displayShows" key={show.id}>
+                <div className="name-btn">
+                  <p className="showName">
+                    {" "}
+                    <span>show name:</span> {show.name}
+                  </p>
+                  <button
+                    className="details-btn"
+                    onClick={() => showDetails(show.id)}
+                  >
+                    show details
+                  </button>
+                </div>
+                <div className="details">
+                  {seasons === ""
+                    ? null
+                    : seasonId === show.id
+                    ? seasons.map((season) => (
+                        <div key={season.id} className="seasons">
+                          <p className="seasonsNumber">
+                            season: {season.number}
+                          </p>
+                        </div>
+                      ))
+                    : null}
 
-                      {casts === ""
-                        ? null
-                        : seasonId === show.id
-                        ? casts.map((cast) => (
-                            <div key={cast.id} className="cast">
-                              <p className="castName">
-                                Actor name: {cast.person.name}
-                              </p>
-                            </div>
-                          ))
-                        : null}
-                    </div>
-                  </div>
-                ))
-              : search.map((show) => (
-                  <div className="displayShows" key={show.id}>
-                    <div className="name-btn">
-                      <p className="showName">
-                        <span>show name:</span>
-                        {show.name}
-                      </p>
-                      <button
-                        className="details-btn"
-                        onClick={() => showDetails(show.id)}
-                      >
-                        show details
-                      </button>
-                    </div>
-                    <div className="details">
-                      {seasons === ""
-                        ? null
-                        : seasonId === show.id
-                        ? seasons.map((season) => (
-                            <div key={season.id} className="seasons">
-                              <p className="seasonsNumber">
-                                season: {season.number}
-                              </p>
-                            </div>
-                          ))
-                        : null}
-
-                      {casts === ""
-                        ? null
-                        : seasonId === show.id
-                        ? casts.map((cast) => (
-                            <div key={cast.id} className="cast">
-                              <p className="castName">Actor name: {cast.person.name}</p>
-                            </div>
-                          ))
-                        : null}
-                    </div>
-                  </div>
-                ))}
+                  {casts === ""
+                    ? null
+                    : seasonId === show.id
+                    ? casts.map((cast) => (
+                        <div key={cast.id} className="cast">
+                          <p className="castName">
+                            Actor name: {cast.person.name}
+                          </p>
+                        </div>
+                      ))
+                    : null}
+                </div>
+              </div>
+            ))}
           </div>
         </div>
-      ) : (
-        <Backdrop
-          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-          open={open}
-        >
-          <CircularProgress color="inherit" />
-        </Backdrop>
-      )}
+      }
+
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={Loading}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
     </>
   );
 }
